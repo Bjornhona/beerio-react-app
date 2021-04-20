4b9df4e712183c7f77a9858816863c8326236b10
Bjornhona <asaeri3@hotmail.com>
2021-04-20 23:39:58 +0200

    explore beers section created

--- src/pages/home/components/beers-section/BeersSection.js
+++ src/pages/home/components/beers-section/BeersSection.js
@@ -3,6 +3,9 @@ import './beersSection.css';
 import Button from '../../../../components/button/Button';
 import MeasureItem from '../measure-item/MeasureItem';
 import {faHeart, faBeer, faSkull} from '@fortawesome/free-solid-svg-icons';
+import waterColorImage from "./watercolor-4629342_1920.png";
+import beerSplashImage from "./water-2748670_1920.png";
+import beerGlassImage from "./beer-3622242_1920.png";
 
 const BeersSection = () => {
   const root = document.querySelector(':root');
@@ -14,10 +17,18 @@ const BeersSection = () => {
         <div className="left-beers-content">
           <h2>Explore the best beers in the world.</h2>
           <h6>Search among thousands of beers worldwide and keep track of your favorites.</h6>
-          <Button link="/play" text="Explore"/>
         </div>
+          {/* <Button className="explore-button" link="/play" text="Explore"/> */}
+          <div className="explore-button">
+        <Button link="/play" text="Explore"/>
+      </div>
 
-        <div className="middle-beers-content"></div>
+        <div className="middle-beers-content">
+          <h1>Explore</h1>
+          <div><img className="water-color-image" src={waterColorImage} alt="water color"/></div>
+          <img className="beer-splash-image" src={beerSplashImage} alt="beer splash"/>
+          <img className="beer-glass-image" src={beerGlassImage} alt="glass of beer"/>
+        </div>
 
         <div className="right-beers-content">
           <MeasureItem 
@@ -40,6 +51,9 @@ const BeersSection = () => {
           />
         </div>
       </div>
+      {/* <div className="explore-button">
+        <Button link="/play" text="Explore"/>
+      </div> */}
     </div>
   )
 }
