0135f6603af070fbfe4a73cb923704b0293f17d0
Bjornhona <asaeri3@hotmail.com>
2021-08-07 10:40:34 +0200

    Brewery screen styled and done

--- src/pages/breweries/Breweries.js
+++ src/pages/breweries/Breweries.js
@@ -11,9 +11,7 @@ import BreweriesItem from './components/breweries-item/BreweriesItemContainer';
 
 const Breweries = () => {
   const [breweriesData, setBreweriesData] = useState([]);
-  // const [breweryData, setBreweryData] = useState([]);
   const [zipCode, setZipCode] = useState("");
-  // const [locationsData, setLocationsData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [inputValue, setInputValue] = useState('');
 
@@ -34,7 +32,6 @@ const Breweries = () => {
 
     const getLocations = () => {
       return beerService.getLocations(zipCode)
-    //  .then(data => setLocationsData(data))
     .then(data => {
       const breweriesResponse = data.map(d => d.brewery)
       const uniqueBreweriesResponse = Array.from(new Set(breweriesResponse.map(a => a.id)))
@@ -63,29 +60,10 @@ const Breweries = () => {
     setInputValue(event.target.value);
   }
 
-  // const fetchBreweryAddress = (event) => {
-  //   const breweryId = event.target.value;
-
-  //   return beerService.getBrewery(breweryId)
-  //   .then(response => setBreweryData(response))
-  //   .catch(error => console.error('Error: No Brewery data received from API'));
-  // }
-
-  // const getLocations = () => {
-  //    return beerService.getLocations(zipCode)
-  //   .then(data => setLocationsData(data.brewery))
-  //   .catch(error => console.error('Error: No Locations data received from API'));
-  // }
-
   const handleInputChange = (event) => {
     setInputValue('');
-    // console.log('New search');
     setZipCode(event.target.value);
-    // getLocations(event.target.value);
   }
-
-console.log(breweriesData);
-// console.log(locationsData && locationsData);
 
   return (
     <>
@@ -93,96 +71,32 @@ console.log(breweriesData);
       <div className="breweries-screen">
         <HeaderSection 
           headline="Start your brewery adventure"
-          breadText="Search amont your favourite breweries to plan your next visit."
+          breadText="Search among your favourite breweries to plan your next visit."
           image={headerImage}/>
 
         <div className="breweries-div outer-content">
           <div className="beers-div-header">
             <h2>Find your brewery.</h2>
           </div>
-          <div className="breweries-search">
-            <FontAwesomeIcon className="search-icon" icon={faSearch}/>
-            <input  type="text"
-                    name="name" 
-                    value={inputValue} 
-                    onChange={handleSearchInput} 
-                    placeholder="Search by name..." />
+
+          <div className="breweries-searchboxes">
+            <div className="breweries-search">
+              <FontAwesomeIcon className="search-icon" icon={faSearch}/>
+              <input  type="text"
+                      name="name" 
+                      value={inputValue} 
+                      onChange={handleSearchInput} 
+                      placeholder="Search by name..." />
+            </div>
+
+            <div className="breweries-search">
+              <FontAwesomeIcon className="search-icon" icon={faSearch}/>
+              <input  type="text" 
+                      value={zipCode} 
+                      onChange={handleInputChange}
+                      placeholder="Search by zip-code..." />
+            </div>
           </div>
-
-          <div className="breweries-search">
-            <FontAwesomeIcon className="search-icon" icon={faSearch}/>
-            <input  type="text" 
-                    value={zipCode} 
-                    onChange={handleInputChange}
-                    placeholder="Search by zip-code..." />
-            {/* <button onClick={() => getLocations()}>Get Location</button> */}
-            {/* {locationsData && locationsData.map(nearbyBrewery => {
-              console.log(nearbyBrewery.brewery)
-              return (
-                <div key={nearbyBrewery.id}>
-                <h4>{nearbyBrewery.brewery.name}, {nearbyBrewery.locality}</h4>
-                  <p>{nearbyBrewery.latitude}</p>
-                  <p>{nearbyBrewery.longitude}</p>
-                </div>
-              )
-            })} */}
-          </div>
-        
-          {/* <h2>Breweries</h2> */}
-          {/* {breweriesData && breweriesData.length !== 0 &&
-          <select id="breweries" name="Breweries" onChange={event => fetchBreweryAddress(event)}> 
-            {breweriesData.map(brewery => 
-                <option key={brewery.id} value={brewery.id}>
-                  {brewery.name}
-                </option>
-            )}
-          </select>} */}
-
-          {/* <div className="beers-search">
-            <FontAwesomeIcon className="search-icon" icon={faSearch}/>
-            <input  type="text"
-                    name="name" 
-                    value={inputValue} 
-                    onChange={handleSearchInput} 
-                    placeholder="Search..." />
-          </div> */}
-
-          {/* {breweryData !== [] && breweryData.map(location => {
-            let breweryInformation = {};
-
-            breweriesData.forEach(brewery => {
-              const breweryInfo = brewery.locations ? brewery.locations.filter(loc => loc.id === location.id) : []
-              
-              if (breweryInfo.length > 0) {
-                breweryInformation = brewery
-              }
-            })
-            console.log(breweryInformation)
-
-            return (
-              <div key={location.id}>
-                <h4>{breweryInformation.name}</h4>
-                <p>{location.latitude}</p>
-                <p>{location.longitude}</p>
-              </div>
-            )
-          })} */}
-
-          {/* <div>
-            <h2>Get nearest Breweries based on your zip-code</h2>
-            <input type="text" placeholder="Enter your zip-code" value={zipCode} onChange={handleInputChange}/>
-            <button onClick={() => getLocations()}>Get Location</button>
-            {locationsData && locationsData.map(nearbyBrewery => {
-              // console.log(nearbyBrewery)
-              return (
-                <div key={nearbyBrewery.id}>
-                <h4>{nearbyBrewery.brewery.name}, {nearbyBrewery.locality}</h4>
-                  <p>{nearbyBrewery.latitude}</p>
-                  <p>{nearbyBrewery.longitude}</p>
-                </div>
-              )
-            })}
-          </div> */}
 
           {breweriesData && breweriesData.length !== 0 ?
             <div className="beers-item-container">
@@ -194,7 +108,7 @@ console.log(breweriesData);
               )}
             </div> :
           <div className="no-response">
-            <p>No beers matched your search.</p>
+            <p>No breweries matched your search.</p>
           </div>}
         
         </div>
--- src/pages/brewery/Brewery.js
+++ src/pages/brewery/Brewery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './brewery.css';
 import BackButton from '../../components/back-button/BackButton';
 
 const Brewery = ({breweryState}) => {
